1791f936e3cd2c671fdc8c5eb47d82175869cc71
mahuaibo <mahuaibo@360baige.com>
2017-06-12 15:35:33 +0800

    ma

---
+++ filters/filter.go
@@ -0,0 +1,35 @@
+package filters
+
+import (
+	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	log := logs.NewLogger()
+	log.SetLogger(logs.AdapterConsole)
+	log.Debug("filter run start")
+
+	beego.InsertFilter("/v1/object/*",beego.BeforeRouter, UrlManager)
+	beego.InsertFilter("/v1/user/*",beego.BeforeRouter, UrlOriginManager)
+
+	log.Debug("filter run end")
+}
+
+var UrlManager = func(ctx *context.Context) {
+	//ctx.Redirect(302, "/v1/user")
+}
+
+var UrlOriginManager = func(ctx *context.Context) {
+	r := ctx.Request
+	w := ctx.ResponseWriter
+	Origin := r.Header.Get("Origin")
+	if Origin != "" {
+		w.Header().Add("Access-Control-Allow-Origin", Origin)
+		w.Header().Add("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE")
+		w.Header().Add("Access-Control-Allow-Headers", "x-requested-with,content-type")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+	}
+	//ctx.Redirect(302, "/v1/user")
+}
---
+++ rpc/client/client.go
@@ -0,0 +1,21 @@
+package client
+
+import (
+	"github.com/smallnest/rpcx"
+	"github.com/smallnest/rpcx/clientselector"
+	"time"
+)
+
+/**
+ * 获取服务
+ */
+func Call(etcdURL, serviceName, methodName string, args, reply interface{}) error {
+	// RandomSelect RoundRobin WeightedRoundRobin ConsistentHash
+	s := clientselector.NewEtcdClientSelector([]string{etcdURL}, "/rpcx/"+serviceName, time.Minute, rpcx.RandomSelect, time.Minute)
+	client := rpcx.NewClient(s)
+	// Failfast Failover Failtry Broadcast Forking
+	client.FailMode = rpcx.Failover
+	err := client.Call(serviceName+"."+methodName, args, &reply)
+	client.Close()
+	return err
+}
