ea5fd2de4c4b76ec72b442b98d2f2d4592a20e50
taihe <taihe@bigwei.com>
2017-06-13 15:21:16 +0800

    th

--- controllers/user.go
+++ controllers/user.go
@@ -11,6 +11,30 @@ type UserController struct {
 	beego.Controller
 }
 
+// @Title 注册账号接口
+// @Description 注册账号接口
+// @Param	page		path 	int	true		"分页页码"
+// @Param	rows		path 	int	true		"展示条数"
+// @Success 200 {object} models.logger
+// @Failure 403 :uid is empty
+// @router /register [post]
+func (c *UserController) Register() {
+	var reply models.User
+	args := &models.User{
+		Username: c.GetString("Username"),
+		Password: c.GetString("Password"),
+	}
+	err := client.Call("http://127.0.0.1:2379", "Admin", "Register", args, &reply)
+	fmt.Println(reply, err)
+	if err == nil {
+		// TODO 注册成功添加角色
+		c.Data["json"] = reply
+	} else {
+		c.Data["json"] = err
+	}
+	c.ServeJSON()
+}
+
 // @Title 登录接口
 // @Description 登录接口
 // @Success 200 {"code":200,"messgae":"ok","data":{"username":"zhangs","accessToken":"ok"}}
@@ -48,6 +72,98 @@ func (c *UserController) Logout() {
 		AccessToken: c.GetString("AccessToken"),
 	}
 	err := client.Call("http://127.0.0.1:2379", "User", "Logout", args, &reply)
+	fmt.Println(reply, err)
+	if err == nil {
+		c.Data["json"] = reply
+	} else {
+		c.Data["json"] = err
+	}
+	c.ServeJSON()
+}
+
+
+// @Title 用户注销接口
+// @Description 用户注销接口
+// @Success 200 {"code":200,"messgae":"ok", "data":{ ... ... }}
+// @Param   Username     query   string true       "用户ID"
+// @Param   AccessToken     query   string true       "访问令牌"
+// @Failure 400 {"code":400,"message":"..."}
+// @router /cancel [post]
+func (c *UserController) Cancel() {
+	var reply models.User
+	args := &models.User{
+		Username: c.GetString("Username"),
+		AccessToken: c.GetString("AccessToken"),
+	}
+	err := plugin.Call("http://127.0.0.1:2379", "Admin", "Cancel", args, &reply)
+	fmt.Println(reply, err)
+	if err == nil {
+		c.Data["json"] = reply
+	} else {
+		c.Data["json"] = err
+	}
+	c.ServeJSON()
+}
+
+// @Title 用户删除接口
+// @Description 用户删除接口
+// @Success 200 {"code":200,"messgae":"ok", "data":{ ... ... }}
+// @Param   Id     query   string true       "用户ID"
+// @Failure 400 {"code":400,"message":"..."}
+// @router /delete/:id [get]
+func (c *UserController) Delete() {
+	id, _ := c.GetInt64("Id")
+	var reply models.User
+	args := &models.User{
+		Id:id,
+	}
+	err := plugin.Call("http://127.0.0.1:2379", "Admin", "Delete", args, &reply)
+	if err == nil {
+		c.Data["json"] = reply
+	} else {
+		c.Data["json"] = err
+	}
+	c.ServeJSON()
+}
+
+// @Title 账号激活接口
+// @Description 账号激活接口
+// @Success 200 {"code":200,"messgae":"ok", "data":{ ... ... }}
+// @Param   Username     query   string true       "用户名称"
+// @Failure 400 {"code":400,"message":"..."}
+// @router /activation/:username [get]
+func (c *UserController) Activation() {
+	var reply models.User
+	args := &models.User{
+		Username:c.GetString("Username"),
+	}
+	err := plugin.Call("http://127.0.0.1:2379", "Admin", "Activation", args, &reply)
+	fmt.Println(reply, err)
+	if err == nil {
+		c.Data["json"] = reply
+	} else {
+		c.Data["json"] = err
+	}
+	c.ServeJSON()
+}
+
+// @Title 用户信息详情
+// @Description 用户信息详情
+// @Success 200 {"code":200,"messgae":"ok", "data":{ ... ... }}
+// @Param   Code     query   string true       "验证码"
+// @Param   Username     query   string true       "用户名称"
+// @Param   Password     query   string true       "用户密码"
+// @Failure 400 {"code":400,"message":"..."}
+// @router /findPassword [post]
+func (c *UserController) FindPassword() {
+	var reply models.User
+	args := &models.User{
+		Username:c.GetString("Username"),
+		Password:c.GetString("password"),
+		Code:c.GetString("Code"),
+		CodeTime:time.Now().UnixNano() / 1e6,
+	}
+	err := plugin.Call("http://127.0.0.1:2379", "Admin", "FindPassword", args, &reply)
 	fmt.Println(reply, err)
 	if err == nil {
 		c.Data["json"] = reply
