a8a85c4645c2aec512c1eb29edd5d6d1dcb92daf
mahuaibo <mahuaibo@360baige.com>
2017-07-26 19:11:14 +0800

    ma

--- rpc/client/client.go
+++ rpc/client/client.go
@@ -4,6 +4,7 @@ import (
 	"github.com/smallnest/rpcx"
 	"github.com/smallnest/rpcx/clientselector"
 	"time"
+	"context"
 )
 
 /**
@@ -16,8 +17,7 @@ func Call(etcdURL, serviceName, methodName string, args, reply interface{}) erro
 	client := rpcx.NewClient(s)
 	// Failfast Failover Failtry Broadcast Forking
 	client.FailMode = rpcx.Failover
-	err := client.Call(serviceName+"."+methodName, args, &reply)
-	//err := client.Call(context.Background(),serviceName+"."+methodName, args, &reply)
+	err := client.Call(context.Background(),serviceName+"."+methodName, args, &reply)
 	client.Close()
 	return err
 }
