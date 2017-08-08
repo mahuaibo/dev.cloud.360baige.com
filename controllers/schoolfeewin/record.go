837bab56ebec855826f8b4865521f2337aeba607
mahuaibo <mahuaibo@360baige.com>
2017-08-08 16:41:47 +0800

    ma

--- controllers/schoolfeewin/record.go
+++ controllers/schoolfeewin/record.go
@@ -101,6 +101,270 @@ func (c *RecordController) ListOfRecord() {
 	return
 }
 
+// @Title 添加收费名单接口
+// @Description Record Add 添加收费名单接口
+// @Success 200 {"code":200,"messgae":"添加收费名单成功","data":{"access_ticket":"xxxx","expire_in":0}}
+// @Param   access_token     query   string true       "访问令牌"
+// @Param   name     query   string true       "项目名称"
+// @Param   is_limit     query   string true       "是否限制缴费"
+// @Param   desc     query   string true       "描述"
+// @Param   link     query   string true       "描述链接"
+// @Param   status     query   string true       "状态 -1注销 0正常"
+// @Failure 400 {"code":400,"message":"添加收费名单失败"}
+// @router /add [post]
+func (c *RecordController) AddRecord() {
+	res := AddRecordResponse{}
+	access_token := c.GetString("access_token")
+	project_id, _ := c.GetInt64("project_id", 0)
+	name := c.GetString("name")
+	class_name := c.GetString("class_name")
+	id_card := c.GetString("id_card")
+	num := c.GetString("num")
+	phone := c.GetString("phone")
+	price, _ := c.GetFloat("price")
+	is_fee, _ := c.GetInt8("is_fee")
+	fee_time, _ := c.GetInt64("fee_time")
+	desc := c.GetString("desc")
+	if access_token == "" {
+		res.Code = ResponseLogicErr
+		res.Messgae = "访问令牌无效"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	// 1.
+	var args action.FindByCond
+	args.CondList = append(args.CondList, action.CondValue{
+		Type: "And",
+		Key:  "access_token",
+		Val:  access_token,
+	})
+	args.Fileds = []string{"id", "user_id", "company_id", "type"}
+	var replyAccessToken user.UserPosition
+	err := client.Call(beego.AppConfig.String("EtcdURL"), "UserPosition", "FindByCond", args, &replyAccessToken)
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "访问令牌失效"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	fmt.Println("1:", replyAccessToken)
+
+	// 2.
+	operationTime := time.Now().UnixNano() / 1e6
+	args2 := &schoolfee.Record{
+		CreateTime: operationTime,
+		UpdateTime: operationTime,
+		CompanyId:  replyAccessToken.CompanyId,
+		ProjectId:  project_id,
+		Name:       name,
+		ClassName:  class_name,
+		IdCard:     id_card,
+		Num:        num,
+		Phone:      phone,
+		Desc:       desc,
+		Price:      price,
+		IsFee:      is_fee,
+		FeeTime:    fee_time,
+		Status:     0,
+	}
+	var replyRecord schoolfee.Record
+	err = client.Call(beego.AppConfig.String("EtcdURL"), "Record", "Add", args2, &replyRecord)
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "添加收费名单失败"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	fmt.Println("2:", replyRecord)
+	res.Code = ResponseNormal
+	res.Messgae = "添加收费名单成功"
+	res.Data.Id = replyRecord.Id
+	c.Data["json"] = res
+	c.ServeJSON()
+	return
+}
+
+// @Title 查看缴费记录接口
+// @Description Project Add 查看缴费记录接口
+// @Success 200 {"code":200,"messgae":"查看缴费记录成功","data":{"access_ticket":"xxxx","expire_in":0}}
+// @Param   access_token     query   string true       "访问令牌"
+// @Param   record_id     query   string true       "记录ID"
+// @Failure 400 {"code":400,"message":"查看缴费记录失败"}
+// @router /detail [get]
+func (c *RecordController) DetailRecord() {
+	res := DetailRecordResponse{}
+	access_token := c.GetString("access_token")
+	record_id, _ := c.GetInt64("record_id", 0)
+	if access_token == "" {
+		res.Code = ResponseLogicErr
+		res.Messgae = "访问令牌无效"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	// 1.
+	var args action.FindByCond
+	args.CondList = append(args.CondList, action.CondValue{
+		Type: "And",
+		Key:  "access_token",
+		Val:  access_token,
+	})
+	args.Fileds = []string{"id", "user_id", "company_id", "type"}
+	var replyAccessToken user.UserPosition
+	err := client.Call(beego.AppConfig.String("EtcdURL"), "UserPosition", "FindByCond", args, &replyAccessToken)
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "访问令牌失效"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	fmt.Println("1:", replyAccessToken)
+
+	// 2.
+	args2 := &schoolfee.Record{
+		Id: record_id,
+	}
+	err = client.Call(beego.AppConfig.String("EtcdURL"), "Record", "FindById", args2, args2)
+
+	fmt.Println("2:", args2)
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "修改缴费项目失败"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	res.Code = ResponseNormal
+	res.Messgae = "修改缴费项目成功"
+	res.Data.Data = Record{
+		Id:         args2.Id,
+		CreateTime: args2.CreateTime,
+		UpdateTime: args2.UpdateTime,
+		CompanyId:  args2.CompanyId,
+		ProjectId:  args2.ProjectId,
+		Name:       args2.Name,
+		ClassName:  args2.ClassName,
+		IdCard:     args2.IdCard,
+		Num:        args2.Num,
+		Phone:      args2.Phone,
+		Status:     args2.Status,
+		Price:      args2.Price,
+		IsFee:      args2.IsFee,
+		FeeTime:    args2.FeeTime,
+		Desc:       args2.Desc,
+	}
+	c.Data["json"] = res
+	c.ServeJSON()
+	return
+}
+
+// @Title 修改缴费记录接口
+// @Description Project Add 修改缴费记录接口
+// @Success 200 {"code":200,"messgae":"修改缴费记录成功","data":{"access_ticket":"xxxx","expire_in":0}}
+// @Param   access_token     query   string true       "访问令牌"
+// @Param   record_id     query   string true       "记录ID"
+// @Failure 400 {"code":400,"message":"修改缴费记录失败"}
+// @router /modify [post]
+func (c *RecordController) ModifyRecord() {
+	res := ModifyRecordResponse{}
+	access_token := c.GetString("access_token")
+	record_id, _ := c.GetInt64("record_id", 0)
+	name := c.GetString("name")
+	class_name := c.GetString("class_name")
+	id_card := c.GetString("id_card")
+	num := c.GetString("num")
+	phone := c.GetString("phone")
+	price, _ := c.GetFloat("price")
+	is_fee, _ := c.GetInt8("is_fee")
+	fee_time, _ := c.GetInt64("fee_time")
+	desc := c.GetString("desc")
+	if access_token == "" {
+		res.Code = ResponseLogicErr
+		res.Messgae = "访问令牌无效"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	// 1.
+	var args action.FindByCond
+	args.CondList = append(args.CondList, action.CondValue{
+		Type: "And",
+		Key:  "access_token",
+		Val:  access_token,
+	})
+	args.Fileds = []string{"id", "user_id", "company_id", "type"}
+	var replyAccessToken user.UserPosition
+	err := client.Call(beego.AppConfig.String("EtcdURL"), "UserPosition", "FindByCond", args, &replyAccessToken)
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "访问令牌失效"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	fmt.Println("1:", replyAccessToken)
+
+	// 2.
+	args2 := &schoolfee.Record{
+		Id: record_id,
+	}
+	err = client.Call(beego.AppConfig.String("EtcdURL"), "Record", "FindById", args2, args2)
+
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "修改缴费项目失败"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+
+	if args2.CompanyId != replyAccessToken.CompanyId {
+		res.Code = ResponseLogicErr
+		res.Messgae = "非法操作"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+
+	// 3.
+	args3 := action.UpdateByIdCond{
+		Id: []int64{args2.Id},
+	}
+	operationTime := time.Now().UnixNano() / 10e6
+	args3.UpdateList = append(args3.UpdateList,
+		action.UpdateValue{Key: "update_time", Val: operationTime},
+		action.UpdateValue{Key: "name", Val: name},
+		action.UpdateValue{Key: "class_name", Val: class_name},
+		action.UpdateValue{Key: "id_card", Val: id_card},
+		action.UpdateValue{Key: "num", Val: num},
+		action.UpdateValue{Key: "phone", Val: phone},
+		action.UpdateValue{Key: "price", Val: price},
+		action.UpdateValue{Key: "is_fee", Val: is_fee},
+		action.UpdateValue{Key: "fee_time", Val: fee_time},
+		action.UpdateValue{Key: "desc", Val: desc},
+	)
+
+	var replyNum action.Num
+	err = client.Call(beego.AppConfig.String("EtcdURL"), "Record", "UpdateById", args3, &replyNum)
+	if err != nil {
+		res.Code = ResponseLogicErr
+		res.Messgae = "修改缴费记录失败"
+		c.Data["json"] = res
+		c.ServeJSON()
+		return
+	}
+	res.Code = ResponseNormal
+	res.Messgae = "修改缴费记录成功"
+	res.Data.Count = replyNum.Value
+	c.Data["json"] = res
+	c.ServeJSON()
+	return
+}
+
 // @Title 删除校园收费记录接口
 // @Description Delete Record 删除校园收费记录接口
 // @Success 200 {"code":200,"messgae":"删除缴费项目记录成功","data":{"access_ticket":"xxxx","expire_in":0}}
@@ -201,7 +465,7 @@ func (c *RecordController) UploadRecord() {
 			if index != 0 {
 				feeTime, _ := strconv.ParseInt(row.Cells[7].Value, 10, 64)
 				price, _ := strconv.ParseFloat(row.Cells[5].Value, 64) //row.Cells[5].Value.(float64)
-				isFee,_ :=  strconv.ParseInt(row.Cells[6].Value, 10, 64)
+				isFee, _ := strconv.ParseInt(row.Cells[6].Value, 10, 64)
 				args2 = append(args2, schoolfee.Record{
 					CreateTime: timestamp,
 					UpdateTime: timestamp,
