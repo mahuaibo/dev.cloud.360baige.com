c442d9bb72b8da128e0aef1ac0a6f2ac0737b170
mahuaibo <mahuaibo@360baige.com>
2017-08-18 00:31:13 +0800

    am

--- rpc/client/client.go
+++ rpc/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"github.com/smallnest/rpcx"
 	"github.com/smallnest/rpcx/clientselector"
+	//"github.com/smallnest/rpcx/codec"
 	"time"
 	"context"
 )
@@ -13,11 +14,13 @@ import (
 func Call(etcdURL, serviceName, methodName string, args, reply interface{}) error {
 	// RandomSelect RoundRobin WeightedRoundRobin ConsistentHash
 	//NewEtcdV3ClientSelector  NewEtcdClientSelector
-	s := clientselector.NewEtcdClientSelector([]string{etcdURL}, "/rpcx/"+serviceName, time.Minute, rpcx.RandomSelect, 10*time.Second)
+	s := clientselector.NewEtcdV3ClientSelector([]string{etcdURL}, "/rpcx/"+serviceName, time.Minute, rpcx.RandomSelect, 10*time.Second)
 	client := rpcx.NewClient(s)
+	//client.ClientCodecFunc = codec.NewGobClientCodec
+
 	// Failfast Failover Failtry Broadcast Forking
 	client.FailMode = rpcx.Failover
-	err := client.Call(context.Background(),serviceName+"."+methodName, args, &reply)
+	err := client.Call(context.Background(), serviceName+"."+methodName, args, &reply)
 	client.Close()
 	return err
 }
